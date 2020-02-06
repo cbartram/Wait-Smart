b47b229ad181c7bf1cb740f682474288d42cd0e0
cbartram <cbartram3@gmail.com>
2020-02-06 16:11:33 -0500

    Added utils file

---
+++ src/utils.js
@@ -0,0 +1,80 @@
+/**
+ * Utils.js
+ * This file contains a list of commonly used functions throughout different
+ * API routes.
+ */
+const moment = require('moment');
+const crypto = require('crypto');
+const request = require('request-promise-native');
+const {
+    CLIENT_ID,
+    CLIENT_SECRET
+} = process.env;
+const { URL } = require('./constants');
+
+/**
+ * Retrieves the universal OAuth access token. The token is valid
+ * for one hour after the retrieval time.
+ */
+const getUniversalAccessToken = async () => {
+    const today = moment.utc().format("ddd, DD MMM YYYY HH:mm:ss") + " GMT";
+
+    const signatureBuilder = crypto.createHmac("sha256", CLIENT_SECRET);
+    signatureBuilder.update(CLIENT_ID + "\n" + today + "\n");
+    const signature = signatureBuilder.digest("base64").replace(/=$/, "=");
+
+    const options = {
+        'method': 'POST',
+        'url': URL,
+        'headers': {
+            'Date': today,
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ apiKey: CLIENT_ID, signature })
+
+    };
+
+    try {
+        const response = await request(options);
+        const { Token } = JSON.parse(response);
+        console.log('[INFO] Successfully retrieved Universal OAuth access_token: ', Token);
+        return Token;
+    } catch(err) {
+        console.log('[ERROR] There was an error retrieving the universal access_token.', err);
+        throw(err);
+    }
+};
+
+
+/**
+ * Returns all points of interest data from the Universal API
+ * @param access_token String the API access token retrieved from the #getUniversalAccessToken() API call
+ * @returns {Promise<any>}
+ */
+const getPointsOfInterest = async (access_token) => {
+    const ridesOptions = {
+        'method': 'GET',
+        'url': URL + '/pointsOfInterest',
+        'headers': {
+            'Date': moment.utc().format("ddd, DD MMM YYYY HH:mm:ss") + " GMT",
+            'Content-Type': 'application/json; charset=UTF-8',
+            'Accept': 'application/json',
+            'X-UNIWebService-ApiKey': 'AndroidMobileApp',
+            'X-UNIWebService-Token': access_token,
+            'Accept-Language': 'en-US'
+        },
+    };
+    try {
+        const response = await request(ridesOptions);
+        return JSON.parse(response);
+    } catch(err) {
+        console.log('[ERROR] There was an error retrieving the universal points of interest.', err);
+        throw(err);
+    }
+};
+
+
+module.exports = {
+    getUniversalAccessToken,
+    getPointsOfInterest,
+};
